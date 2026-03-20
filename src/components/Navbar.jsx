8ef6e7a24e478dbf6c1e2d5104292f7a6f95f6ae
Mohan30-7 <mohanavijayanj@gmail.com>
2026-03-20 11:33:53 +0530

    Update logo to cursive Mj with cyan-purple gradient

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
     <header className="navbar">
       <div className="navbar-inner">
         <div className="logo" onClick={() => handleScroll('home')}>
-          <span className="logo-name">MJ</span>
+          <span className="logo-name">Mj</span>
           <span style={{ marginLeft: '6px', fontSize: '1.1rem', fontWeight: 600 }}>Portfolio</span>
         </div>
 
